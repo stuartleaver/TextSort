cbeed35eb420d26bd7ca357dacaad08797ee1caa
Pierre Avenant <pierre.avenant@gmail.com>
2022-03-22 20:53:02 +0000

    add unit tests

--- IntegrationTest/UnitTest1.cs
+++ IntegrationTest/UnitTest1.cs
@@ -7,6 +7,24 @@ namespace IntegrationTest
     public class UnitTest1
     {
         [TestMethod]
+        public void WordSort()
+        {        
+            Assert.AreEqual("Boom Zoom", SimpleTest.MyTest.CalculateTotal("Zoom Boom"));
+        }
+
+        [TestMethod]
+        public void CaseSort()
+        {        
+            Assert.AreEqual("Boom boom", SimpleTest.MyTest.CalculateTotal("boom Boom"));
+        }
+
+         [TestMethod]
+        public void RemoveInvalidChars()
+        {        
+            Assert.AreEqual("b b", SimpleTest.MyTest.CalculateTotal("b, b"));
+        }
+
+        [TestMethod]
         public void SimpleTest1()
         {        
             Assert.AreEqual("baby Go go", SimpleTest.MyTest.CalculateTotal("Go baby, go"));
@@ -16,6 +34,6 @@ namespace IntegrationTest
         public void SimpleTest2()
         {
             Assert.AreEqual("ABC aBc abc CBA CBA cba", SimpleTest.MyTest.CalculateTotal("CBA, abc aBc ABC cba CBA."));
-        }
+        
     }
 }
